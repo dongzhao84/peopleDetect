b6d416d75e5cdf06a9dfd53106931e315fdb5699
dongzhao84 <m18092846901@163.com>
2017-10-18 15:13:49 +0800

    Initial Commit

---
+++ PedestrianDetection_Acf_re/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PedestrianDetection_Acf_re
+//
+//  Created by 董昭 on 17/10/18.
+//  Copyright © 2017年 董昭. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
